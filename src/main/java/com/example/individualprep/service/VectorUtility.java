db7de6ecdd6a666dd872f2d181e044986371fea2
RousanChandra <syahrousan@gmail.com>
2026-02-13 19:22:52 +0700

    Implement divide method

--- src/main/java/com/example/individualprep/service/VectorUtility.java
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class VectorUtility {
-    
+
     public double[] add(double[] v1, double[] v2) {
         // TODO: Implement me properly!
         return new double[] { 0.0, 0.0, 0.0 };
@@ -19,19 +19,12 @@ public class VectorUtility {
         // TODO: Implement me properly!
         return new double[] { 0.0, 0.0, 0.0 };
     }
-    
+
     public double dotProduct(double[] v1, double[] v2) {
         // TODO: Implement me properly!
-        if (v1.length != v2.length){
-            throw new IllegalArgumentException("Vector length must be equal");
-        }
-        double result = 0;
-        for(int i = 0 ; i <v1.length ; i++){
-            result += v1[i]*v2[i];
-        }
-        return result;
+        return 0.0;
     }
-    
+
     public double norm(double[] v1) {
         // TODO: Implement me properly!
         return 0.0;
