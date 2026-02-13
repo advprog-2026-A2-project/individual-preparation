66a43e02fdcc3e33ebe698225da6c568484fae91
RousanChandra <syahrousan@gmail.com>
2026-02-13 19:25:45 +0700

    Implement divide method with checkstyle

--- src/main/java/com/example/individualprep/service/ArithmeticUtility.java
+++ src/main/java/com/example/individualprep/service/ArithmeticUtility.java
@@ -2,33 +2,64 @@ package com.example.individualprep.service;
 
 import org.springframework.stereotype.Service;
 
+/** Utility class for arithmetic operations.*/
 @Service
-public class ArithmeticUtility {
-    
-    public double add(double o1, double o2) {
+public final class ArithmeticUtility {
+
+    /**
+     * Adds two numbers.
+     * @param o1 first number
+     * @param o2 second number
+     * @return result of addition
+     */
+    public double add(final double o1, final double o2) {
         // TODO: Implement me properly!
         return 0.0;
     }
 
-    public double subtract(double o1, double o2) {
+    /**
+     * Subtracts two numbers.
+     * @param o1 first number
+     * @param o2 second number
+     * @return result of subtraction
+     */
+    public double subtract(final double o1, final double o2) {
         // TODO: Implement me properly!
         return 0.0;
     }
 
-    public double multiply(double o1, double o2) {
+    /**
+     * Multiplies two numbers.
+     * @param o1 first number
+     * @param o2 second number
+     * @return result of multiplication
+     */
+    public double multiply(final double o1, final double o2) {
         // TODO: Implement me properly!
         return 0.0;
     }
 
-    public double divide(double o1, double o2) {
-        // TODO: Implement me properly!
+    /**
+     * Divides two numbers.
+     * @param o1 first number
+     * @param o2 second number
+     * @return result of division
+     * @throws ArithmeticException if dividing by zero
+     */
+    public double divide(final double o1, final double o2) {
         if (o2 == 0.0) {
             throw new ArithmeticException("Cannot divide by zero");
         }
         return o1 / o2;
     }
 
-    public double exponent(double o1, int n) {
+    /**
+     * Computes the exponentiation of a number.
+     * @param o1 base
+     * @param n exponent
+     * @return result of exponentiation
+     */
+    public double exponent(final double o1, final int n) {
         // TODO: Implement me properly!
         return 0.0;
     }
