0f14d4a29810bfec3289521d32349c9dd15b96a2
Yufanjonni <muhammad.yufan@ui.ac.id>
2026-02-13 19:36:05 +0700

    clean code

--- src/main/java/com/example/individualprep/service/VectorUtility.java
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -6,30 +6,26 @@ import org.springframework.stereotype.Service;
 public class VectorUtility {
     
     public double[] add(double[] v1, double[] v2) {
-        // TODO: Implement me properly!
         return new double[] { 0.0, 0.0, 0.0 };
     }
 
     public double[] subtract(double[] v1, double[] v2) {
-        // TODO: Implement me properly!
         return new double[] { 0.0, 0.0, 0.0 };
     }
 
     public double[] multiply(double[] v1, int x) {
         double[] result = new double[v1.length];
-        for(int i = 0; i < v1.length; i++){
+        for(int i = 0; i < v1.length; i++) {
             result[i] = v1[i] * x;
         }
         return result;
     }
     
     public double dotProduct(double[] v1, double[] v2) {
-        // TODO: Implement me properly!
         return 0.0;
     }
     
     public double norm(double[] v1) {
-        // TODO: Implement me properly!
         return 0.0;
     }
 }
