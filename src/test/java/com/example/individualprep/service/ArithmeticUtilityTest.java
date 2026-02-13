3a64a0be5616e06be49f4a3ee235b7e9a2c3b57e
Yufanjonni <muhammad.yufan@ui.ac.id>
2026-02-13 18:58:45 +0700

    deleting unused import

--- src/test/java/com/example/individualprep/service/ArithmeticUtilityTest.java
+++ src/test/java/com/example/individualprep/service/ArithmeticUtilityTest.java
@@ -1,10 +1,8 @@
 package com.example.individualprep.service;
 
-import com.example.individualprep.service.ArithmeticUtility;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ArithmeticUtilityTest {
 
